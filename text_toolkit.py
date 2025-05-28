93c2d111432d05e0954e29695de0bf5bd348ce7c
Sagar Parmar <sagarparmar881@gmail.com>
2025-05-28 08:34:06 +0530

    feat :: Reverted text_toolkit.py file.

--- text_toolkit.py
+++ text_toolkit.py
@@ -3,8 +3,3 @@ def placeholder():
 
 def word_count(text: str) -> int:
     return len(text.split())
-
-def to_upper(text: str) -> str:
-    #This is commit one.
-    #This is commit two.
-    return text.upper()
